ccd7df5b12415eb9fe21130f2e6b9a253084b57a
Henry Young <8026353+heyoung@users.noreply.github.com>
2022-04-02 15:15:00 +0100

    add jobs module to app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { JobsModule } from './jobs/jobs.module'
 
 @Module({
-  imports: [],
+  imports: [
+    // TODO: push the connection user + pwd to be in config. Do we need to set the user up in docker compose too?
+    MongooseModule.forRoot('mongodb://root:root@localhost:27017/'),
+    JobsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
