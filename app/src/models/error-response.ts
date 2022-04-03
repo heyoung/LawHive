b8cd73f43ae92ec7c9e198cf2591102e771ea10a
Henry Young <8026353+heyoung@users.noreply.github.com>
2022-04-03 17:12:30 +0100

    add todo

--- app/src/models/error-response.ts
+++ app/src/models/error-response.ts
@@ -1,3 +1,5 @@
+// TODO: nice to have this as a class with a tostring method that handles
+//       getting the complete messeage when message is undefined or not etc.
 export default interface ErrorResponse {
   status: number
   error: string
