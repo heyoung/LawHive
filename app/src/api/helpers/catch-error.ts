56efe9cc16bb5edfd41ffe9ad9af80f87a56a732
Henry Young <8026353+heyoung@users.noreply.github.com>
2022-04-03 16:32:41 +0100

    add todo

--- app/src/api/helpers/catch-error.ts
+++ app/src/api/helpers/catch-error.ts
@@ -3,6 +3,8 @@ import * as E from 'fp-ts/lib/Either'
 import axios from 'axios'
 import OkResponse from '../../models/ok-response'
 
+// TODO: the error response has json of the same format as ErrorResponse
+//       we can parse this to get more detailed error messages vs the axios messages
 export async function sendRequest<T>(
   fn: () => Promise<OkResponse<T>>,
 ): Promise<E.Either<T, ErrorResponse>> {
